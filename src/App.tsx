62c1756fed27010342d7a558f8a5e52a3e21dafd
Matias Saade <matiassaade@gmail.com>
2024-04-03 20:51:50 -0300

    starting project, first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+    <div className="container">
+        <h1 className="border-2 rounded-md p-3 text-white">Reservas App</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
